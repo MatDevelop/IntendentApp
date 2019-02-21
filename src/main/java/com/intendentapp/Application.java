299a0c3ceafed9c4ea558385dac3af908941487a
Mateusz Brugier <mateusz.brugier@gmail.com>
2019-02-21 13:00:06 +0100

    Dodanie loga startu aplikacji

--- src/main/java/com/intendentapp/Application.java
+++ src/main/java/com/intendentapp/Application.java
@@ -1,11 +1,19 @@
 package com.intendentapp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.intendentapp.controller.MainController;
+
 @SpringBootApplication
-public class Application{
+public class Application {
+	
+	private static final Logger log = LogManager.getLogger(Application.class);
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("Start aplikacji...");
     }
 }
